49be692120d2e18f1fee9b50daf8ce2bf24cfd0e
Tharun Gajula <tharun.gajula.2@gmail.com>
2025-04-10 01:47:54 +0530

    fix: removed export mode to restore Tailwind, layout, and fonts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/tharun-kumar-portfolio',
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true, // ✅ This is the magic
   },
